5148064b1f46942b2e82a17a444e76241a2519a9
Tyson Paul <tysonpaul89@gmail.com>
2020-11-28 07:24:49 +0530

    scribble database config added and /books and /book route changes

--- go.mod
+++ go.mod
@@ -6,6 +6,6 @@ require (
 	github.com/google/uuid v1.1.2
 	github.com/gorilla/mux v1.8.0
 	github.com/jcelliott/lumber v0.0.0-20160324203708-dd349441af25 // indirect
-	github.com/nanobox-io/golang-scribble v0.0.0-20190309225732-aa3e7c118975 // indirect
+	github.com/nanobox-io/golang-scribble v0.0.0-20190309225732-aa3e7c118975
 	github.com/spf13/viper v1.7.1
 )
--- main.go
+++ main.go
@@ -1,22 +1,26 @@
 package main
 
 import (
+	// "bytes"
+	// "encoding/gob"
 	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 
-	"github.com/google/uuid"
+	// "github.com/google/uuid"
 	"github.com/gorilla/mux"
+	"github.com/nanobox-io/golang-scribble"
 	"github.com/spf13/viper"
 
 	"github.com/tysonpaul89/go-gorilla-mux-example/middleware"
 	"github.com/tysonpaul89/go-gorilla-mux-example/models"
 )
 
-var books []models.Book
+var db *scribble.Driver
 
 func main() {
+	// ======================== Getting configurations =========================
 	// Read and loads the config data
 	viper.SetConfigName("config") // name of config file (without extension)
 	viper.SetConfigType("json")
@@ -26,6 +30,20 @@ func main() {
 		panic(fmt.Errorf("Fatal error config file: %s", err))
 	}
 
+	// // To Adding some sample data
+	// books := []models.Book{}
+	// books = append(books, models.Book{ID: uuid.UUID.String(uuid.New()), Title: "Song of Ice and Fire", Price: 20.5, Author: &models.Author{Firstname: "George", Lastname: "R. R. Martin"}})
+	// books = append(books, models.Book{ID: uuid.UUID.String(uuid.New()), Title: "The Lord of the Rings", Price: 23.8, Author: &models.Author{Firstname: "J.R.R.", Lastname: "Tolkien"}})
+	// db = getDatabaseDriver()
+	// for _, book := range books {
+	// 	fmt.Println(book)
+	// 	db.Write(
+	// 		viper.GetString("database.name"),
+	// 		book.ID,
+	// 		book,
+	// 	)
+	// }
+	// ======================== Common Middlewares =============================
 	// Getting router object.
 	r := mux.NewRouter()
 
@@ -33,33 +51,70 @@ func main() {
 	r.Use(middleware.LoggingMiddleware)
 	r.Use(middleware.JsonHeaderMiddleware)
 
+	// ======================== Routes =========================================
 	// Definding route and route method.
 	r.HandleFunc("/books", getBooks).Methods("GET")
 	r.HandleFunc("/book/{id}", getBook).Methods("GET")
 
-	// Adding some sample data
-	books = append(books, models.Book{ID: uuid.UUID.String(uuid.New()), Title: "Song of Ice and Fire", Price: 20.5, Author: &models.Author{Firstname: "George", Lastname: "R. R. Martin"}})
-	books = append(books, models.Book{ID: uuid.UUID.String(uuid.New()), Title: "The Lord of the Rings", Price: 23.8, Author: &models.Author{Firstname: "J.R.R.", Lastname: "Tolkien"}})
-
+	// ======================== Server Configs =================================
 	// Runs the server in the 8000 port.
 	// Use localhost:8000 to access the URL.
 	log.Fatal(http.ListenAndServe(viper.GetString("port"), r))
 }
 
+func getDatabaseDriver() *scribble.Driver {
+	// a new scribble driver, providing the directory where it will be writing to,
+	// and a qualified logger if desired
+	db, err := scribble.New(viper.GetString("database.path"), nil)
+	if err != nil {
+		panic(fmt.Errorf("Fatal error config file: %s", err))
+	}
+
+	return db
+}
+
 // Gets all the books
 func getBooks(w http.ResponseWriter, r *http.Request) {
+	// Gets the database object
+	db := getDatabaseDriver()
+
+	// Get all the data from the database.
+	data, err := db.ReadAll(
+		viper.GetString("database.name"),
+	)
+
+	if err != nil {
+		panic(err)
+	}
+
+	// Gets all items from database and appends it to books slice.
+	books := []models.Book{}
+	for _, b := range data {
+		book := models.Book{}
+		// Converts each item into byte object.
+		if err := json.Unmarshal([]byte(b), &book); err != nil {
+			panic(err)
+		}
+		books = append(books, book)
+	}
+
 	// This will convert books slice into a json string and writes into the http.ResponseWriter
 	json.NewEncoder(w).Encode(books)
 }
 
 // Get a given book by id
 func getBook(w http.ResponseWriter, r *http.Request) {
+	// Gets the database object
+	db := getDatabaseDriver()
+
+	// Gets the parameters passed from the URL
 	params := mux.Vars(r)
-	for _, book := range books {
-		if book.ID == params["id"] {
-			json.NewEncoder(w).Encode(book)
-			return
-		}
+
+	// Gets an item from the database
+	book := models.Book{}
+	err := db.Read(viper.GetString("database.name"), params["id"], &book)
+	if err != nil {
+		panic(err)
 	}
-	json.NewEncoder(w).Encode(&models.Book{})
+	json.NewEncoder(w).Encode(book)
 }
